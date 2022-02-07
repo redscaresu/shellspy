a6edfd35e6faa50c4c5140c84f98f8464114210c
redscaresu <redscaresu@users.noreply.github.com>
2022-02-07 16:23:52 +0000

    temporary dir

--- shellspy_test.go
+++ shellspy_test.go
@@ -8,6 +8,7 @@ import (
 	"shellspy"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -43,9 +44,18 @@ func TestWriteShellScript(t *testing.T) {
 	session := shellspy.NewSession()
 
 	session.Input = strings.NewReader("echo hello world")
-	session.File = shellspy.CreateFile()
+	tempDir := t.TempDir()
+
+	now := time.Now()
+	filename := tempDir + "shellspy-" + now.Format("2006-01-02-15:04:05") + ".txt"
+	file, err := os.OpenFile(filename,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		t.Fatal("unable to create file")
+	}
+
+	session.File = file
 	session.Run()
-	os.Remove(session.File.Name())
 	fmt.Fprint(gotBuf, session.TranscriptOutput)
 
 	want := wantBuf.String()
