42d742260cd82092cd9824ce9e43bad8699ce3ef
redscaresu <redscaresu@users.noreply.github.com>
2022-04-13 13:45:38 +0100

    remove unnecessary tests

--- shellspy_test.go
+++ shellspy_test.go
@@ -64,58 +64,6 @@ func TestRunCommand(t *testing.T) {
 	}
 }
 
-// func TestRunCommand(t *testing.T) {
-
-// 	cmd := exec.Command("echo", "hello world")
-// 	want := "hello world\n"
-// 	got, _ := shellspy.RunFromCmd(cmd)
-
-// 	if !cmp.Equal(want, got) {
-// 		t.Error(cmp.Diff(want, got))
-// 	}
-
-// 	cmd = exec.Command("pwd", "-x")
-// 	want = "pwd: illegal option -- x\nusage: pwd [-L | -P]\n"
-
-// 	_, got = shellspy.RunFromCmd(cmd)
-// 	if !cmp.Equal(want, got) {
-// 		t.Error(cmp.Diff(want, got))
-// 	}
-
-// }
-
-// func TestWriteShellScript(t *testing.T) {
-
-// 	wantBuf := &bytes.Buffer{}
-// 	gotBuf := &bytes.Buffer{}
-// 	wantBuf.WriteString("hello world\n")
-// 	session, _ := shellspy.NewSession(
-// 		shellspy.WithTranscriptOutput(wantBuf),
-// 	)
-
-// 	session.Input = strings.NewReader("echo hello world")
-// 	tempDir := t.TempDir()
-
-// 	now := time.Now()
-// 	filename := tempDir + "shellspy-" + now.Format("2006-01-02-15:04:05") + ".txt"
-// 	file, err := os.OpenFile(filename,
-// 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-// 	if err != nil {
-// 		t.Fatal("unable to create file")
-// 	}
-
-// 	session.File = file
-// 	session.Run()
-// 	fmt.Fprint(gotBuf, session.Transcript)
-
-// 	want := wantBuf.String()
-// 	got := gotBuf.String()
-
-// 	if !cmp.Equal(want, got) {
-// 		t.Error(cmp.Diff(want, got))
-// 	}
-// }
-
 // func TestRunWithoutPortFlagRunInteractively(t *testing.T) {
 
 // 	buf := &bytes.Buffer{}
